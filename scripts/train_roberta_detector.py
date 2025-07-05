7fa6fbda30b856d9bcfd6fcf4db71ee420604ee4
hoanghapham <hoangha.20021992@gmail.com>
2025-07-05 11:58:11 +0700

    update

--- scripts/train_roberta_detector.py
+++ scripts/train_roberta_detector.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
 
     PROJECT_DIR = Path(__file__).parent.parent
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
+    CACHED_DATA_DIR = PROJECT_DIR / "cache"
+    MODEL_NAME = "Unbabel/xlm-roberta-comet-small"
+    
     # Load data
     x_train_raw = read_text_lines(PROJECT_DIR / "data/wili-2018/x_train.txt")
     y_train_raw = read_text_lines(PROJECT_DIR / "data/wili-2018/y_train.txt")
@@ -32,18 +34,7 @@ if __name__ == "__main__":
     def encode_labels(example):
         example["label"] = label2id[example["label"]]
         return example
-
-
-    train_dict = {"text": x_train_raw, "label": y_train_raw}
-    test_dict = {"text": x_test_raw, "label": y_test_raw}
-
-    train_ds = Dataset.from_dict(train_dict)
-    test_ds = Dataset.from_dict(test_dict)
-
-    # Encode label to numbers
-    train_ds = train_ds.map(encode_labels)
-    test_ds = test_ds.map(encode_labels)
-
+    
     # Tokenize text
     tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-base")
 
@@ -51,26 +42,51 @@ if __name__ == "__main__":
         return tokenizer(example["text"], truncation=True, padding="max_length", max_length=128)
 
 
+    if (CACHED_DATA_DIR / "toknized_train").exists() and (CACHED_DATA_DIR / "tokenized_test").exists():
+
+
+        train_dict = {"text": x_train_raw, "label": y_train_raw}
+        test_dict = {"text": x_test_raw, "label": y_test_raw}
+
+        train_ds = Dataset.from_dict(train_dict)
+        test_ds = Dataset.from_dict(test_dict)
+
+        # Encode label to numbers
+        train_ds = train_ds.map(encode_labels)
+        test_ds = test_ds.map(encode_labels)
+
+
+        # Tokenize texts
+        tokenized_train = train_ds.map(tokenize, batched=True)
+        tokenized_test = test_ds.map(tokenize, batched=True)
+
+        tokenized_train.save_to_disk(CACHED_DATA_DIR / "tokenized_train")
+        tokenized_test.save_to_disk(CACHED_DATA_DIR / "tokenized_test")
+    
+    else:
+        print("Load data")
+        tokenized_train = Dataset.load_from_disk(CACHED_DATA_DIR / "tokenized_train")
+        tokenized_test = Dataset.load_from_disk(CACHED_DATA_DIR / "tokenized_test")
+
     # Load model
+    print("Load model")
     model = XLMRobertaForSequenceClassification.from_pretrained(
-        "xlm-roberta-base",
+        MODEL_NAME,
         num_labels=num_labels,
         id2label=id2label,
         label2id=label2id,
     )
 
-    # Tokenize texts
-    tokenized_train = train_ds.map(tokenize, batched=True)
-    tokenized_test = test_ds.map(tokenize, batched=True)
-
+    
+    print("Start training")
     # Setup & train
     training_args = TrainingArguments(
         output_dir=str(PROJECT_DIR / "models"),
         eval_strategy="epoch",
         save_strategy="epoch",
         learning_rate=2e-5,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=32,
+        per_device_train_batch_size=10,
+        per_device_eval_batch_size=10,
         num_train_epochs=10,
         weight_decay=0.01,
         logging_dir=str(PROJECT_DIR / "logs"),
@@ -88,5 +104,5 @@ if __name__ == "__main__":
     trainer.train()
 
     # Save
-    trainer.save_model("xlmr-lang-detector")
-    tokenizer.save_pretrained("xlmr-lang-detector")
+    trainer.save_model(PROJECT_DIR / "xlm-roberta-small")
+    tokenizer.save_pretrained(PROJECT_DIR / "xlm-roberta-small")
